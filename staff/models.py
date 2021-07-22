0d48ac1f5a603e7ee0191a14c348fd2f0cbb3c88
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-07-22 19:53:11 +0300

    create StaffArchive

--- staff/models.py
+++ staff/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.db.models.fields import DateField, EmailField
-from django.db.models.fields.related import ForeignKey
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -8,7 +7,7 @@ class Staff(models.Model):
     last_name = models.CharField(max_length=20, help_text="Введите фамилию")
     first_name = models.CharField(max_length=20, help_text="Введите имя")
     middle_name = models.CharField(max_length=20, help_text="Введите отчество")
-    rank = ForeignKey('Rank', on_delete=models.SET_NULL, null=True)
+    rank = models.ForeignKey('Rank', on_delete=models.SET_NULL, null=True)
     telegram_nickname = models.CharField(max_length=20,
                                          help_text="Введите никнейм Telegram",
                                          unique=True)
@@ -40,3 +39,24 @@ class Rank(models.Model):
     class Meta:
         verbose_name = "Rank"
         verbose_name_plural = "Rank"
+
+
+class StaffArchive(models.Model):
+    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    middle_name = models.CharField(max_length=20)
+    rank = models.CharField(max_length=20)
+    telegram_nickname = models.CharField(max_length=20, unique=True)
+    phone_number = PhoneNumberField(max_length=14, region=None)
+    personal_email = EmailField(max_length=50, unique=True)
+    work_email = EmailField(max_length=50, unique=True)
+    employment_date = DateField()
+    date_of_birth = DateField()
+    date_of_dismissal = DateField()
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}, {self.rank}'
+
+    class Meta:
+        verbose_name = "Staff Archive"
+        verbose_name_plural = "Staff Archive"
