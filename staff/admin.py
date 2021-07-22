9831cc22ae0726c97c26c9d80c6f3af33e5cec1b
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-07-22 11:45:05 +0300

    create admin Staff, Rank

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from staff.models import Rank, Staff
 
-# Register your models here.
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'middle_name', 'rank',
+                    'telegram_nickname', 'phone_number', 'personal_email',
+                    'work_email', 'employment_date', 'date_of_birth')
+
+    list_filter = ('rank', 'employment_date')
+
+
+@admin.register(Rank)
+class RankAdmin(admin.ModelAdmin):
+    list_display = ['rank']
