12b046675691b9cbbb09790762afa20ddb822c9d
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-07-22 19:55:00 +0300

    create admin Tasks

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tasks.models import Tasks
 
-# Register your models here.
+
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'sender_id', 'addressee_id', 'send_datetime',
+                    'closed')
