6d74c384e3e9e8526d56cd9920ba978a40a3ad20
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-07-28 18:38:28 +0300

    create Tasks, TasksArchive

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import datetime
+from django.utils.timezone import now
 
 
 class Tasks(models.Model):
@@ -10,13 +10,13 @@ class Tasks(models.Model):
                                on_delete=models.SET_NULL, null=True, )
     addressee = models.ForeignKey('staff.Staff', related_name='addressee',
                                   on_delete=models.SET_NULL, null=True)
-    send_datetime = models.DateTimeField(default=datetime.now, blank=True,
-                                         help_text="Дата сообщения")
+    send_datetime = models.DateTimeField(default=now, editable=False)
     closed = models.BooleanField()
 
     def __str__(self):
-        return (f'<{self.subject} {self.sender} {self.addressee}'
-                f'{self.send_datetime} {self.closed}>')
+        return (f'<{self.subject} {self.sender}'
+                f'{self.addressee} {self.send_datetime} {self.closed}'
+                f'{self.content}>')
 
     class Meta:
         verbose_name = "Task"
@@ -27,8 +27,8 @@ class TasksArchive(models.Model):
     subject = models.CharField(max_length=150, help_text="Тема сообщения")
     content = models.TextField(help_text="Текст сообщения")
     result = models.TextField(help_text="Результат", null=True, blank=True)
-    sender = models.CharField(max_length=100) #ФИО отправителя, предварительно нужно преобразовать в строку
-    addressee = models.CharField(max_length=100) #ФИО получателя, предварительно нужно преобразовать в строку
+    sender = models.CharField(max_length=100) # ФИО отправителя, предварительно нужно преобразовать в строку
+    addressee = models.CharField(max_length=100) # ФИО получателя, предварительно нужно преобразовать в строку
     send_datetime = models.DateTimeField(help_text="Дата сообщения")
     closed = models.BooleanField()
 
