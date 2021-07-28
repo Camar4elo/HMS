7828a49c178e0519955d8a74c8bf2d72276a12db
Daniil Kornilov <korniloff.daniil@gmail.com>
2021-07-28 18:35:31 +0300

    create StaffView, StaffDetailView

--- staff/views.py
+++ staff/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from staff.models import Staff
+from django.views.generic.base import View
+from django.views import generic
 
-# Create your views here.
+
+class StaffView(View):
+    def get(self, request):
+        staff = Staff.objects.all()
+        return render(request, "staff_list.html", {"staff": staff})
+
+
+class StaffDetailView(generic.DetailView):
+    def get(self, request, pk):
+        employee = Staff.objects.filter(pk=pk).first()
+        return render(request, "staff_detail.html", {"employee": employee})
